4160cccb7d8d65987277b730133cc0ae2a1bbf64
Matheus de Alencar <matheusdealencar.contato@gmail.com>
2025-01-30 19:40:26 -0300

    feature: restrict users

--- main.py
+++ main.py
@@ -17,18 +17,30 @@ async def ola(ctx):
     await ctx.send('Olá, caro cliente! Estou pronto para uso. Caso queira entender meu funcionamento, utilize o comando "!ajuda".')
 
 @client.command()
+async def segredo(ctx):
+    await ctx.send('Bot segredinho. Shhhhh! -_-')
+
+@client.command()
 async def ajuda(ctx):
-    await ctx.send('**Vou te explicar o que sou e como você pode me usar.**\n\nVeja bem, eu sou um bot que gera itens aleatórios para você usar em suas aventuras de RPG. Para isso, basta digitar o comando **!item** e eu vou te dar um item aleatório. Simples assim! Espero que você goste e se divirta com os itens que eu vou te dar. Boa sorte!\n\nQuer uma curiosidade aleatória? O comando !curiosidade está sendo desenvolvido para gerar curiosidades aleatórias para você!')
+    await ctx.send('**Vou te explicar o que sou e como você pode me usar.**\n\nVeja bem, eu sou um bot que gera itens aleatórios para você usar em suas aventuras de RPG. Para isso, basta digitar o comando **!item** e eu vou te dar um item aleatório. Simples assim! Espero que você goste e se divirta com os itens que eu vou te dar. Boa sorte!\n\nQuer uma curiosidade aleatória? Use o comando **!curiosidade** e eu vou te contar algo que você talvez não saiba. Divirta-se!')
 
 @client.command()
 async def item(ctx):
-    item = return_gp()
-    await ctx.send(f'**Ding ding ding! Você ganhou o item a seguir:**\n\n**->** {item}')
+    user_id = str(ctx.author.id)
+    if check_limit(user_id, "items"):
+        item = return_gp()
+        await ctx.send(f'**Ding ding ding! Você ganhou o item a seguir:**\n\n**->** {item}')
+    else:
+        await ctx.send('Você já pegou muitos itens hoje. Volte amanhã!')
 
-#@client.command() Still in development
-#async def curiosidade(ctx):
-#    curiosidade = return_sp()
-#    await ctx.send(f'Está na hora da cursiosidade aleatória! Será que você já sabia?\n\n-> {curiosidade}')
+@client.command()
+async def curiosidade(ctx):
+    user_id = str(ctx.author.id)
+    if check_limit(user_id, "curiosities"):
+        curiosidade = return_sp()
+        await ctx.send(f'**Está na hora da cursiosidade aleatória! Será que você já sabia?**\n\n-> {curiosidade}')
+    else:
+        await ctx.send('Você já sabe coisas demais. Volte amanhã!')
 
 
 client.run(mykey)
